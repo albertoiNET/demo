0da510ebe3cc4bd31c8cd5e41d11d31d76c1411f
albertoiNET <correo@albertoi.net>
2018-02-25 15:47:52 +0100

    Cleanup

--- src/main/java/net/albertoi/restapp/model/Content.java
+++ src/main/java/net/albertoi/restapp/model/Content.java
@@ -1,21 +1,12 @@
 package net.albertoi.restapp.model;
 
-public class Content{
+public class Content {
     private final long id;
     private final String content;
 
 
-        public Content(long id, String content) {
-            this.id = id;
-            this.content = content;
-        }
-
-
-        public long getId() {
-            return id;
-        }
-
-        public String getContent() {
-            return content;
-        }
+    public Content(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
 }
