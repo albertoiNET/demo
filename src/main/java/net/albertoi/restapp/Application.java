f02e3fc5b5e35ee00ea798d084bbcd3c2566171a
albertoiNET <correo@albertoi.net>
2018-02-25 16:31:50 +0100

    Added GPG commit sign

--- src/main/java/net/albertoi/restapp/Application.java
+++ src/main/java/net/albertoi/restapp/Application.java
@@ -3,6 +3,9 @@ package net.albertoi.restapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring boot app
+ */
 @SpringBootApplication
 public class Application {
 
