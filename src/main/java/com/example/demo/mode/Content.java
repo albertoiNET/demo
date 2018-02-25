58f3cb749f28fa84f5e18976c58d3fca013f5cc2
albertoiNET <correo@albertoi.net>
2018-02-25 14:34:34 +0100

    Content model

---
+++ src/main/java/com/example/demo/mode/Content.java
@@ -0,0 +1,21 @@
+package com.example.demo.mode;
+
+public class Content{
+    private final long id;
+    private final String content;
+
+
+        public Content(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+}
